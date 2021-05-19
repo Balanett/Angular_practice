c4565a829d35e72f2bcae004814759118133a333
Balázs Anett <71498781+Balanett@users.noreply.github.com>
2021-05-19 20:34:22 +0200

    Simple Blog - v6 - finished

--- simple-blog/src/app/app-routing.module.ts
+++ simple-blog/src/app/app-routing.module.ts
@@ -1,28 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule, Routes} from '@angular/router';
-import {PostDetailComponent} from './post-detail/post-detail.component';
-import {NotFoundComponent} from './not-found/not-found.component';
-import {BlogComponent} from './posts/blog/blog.component';
-import {PageComponent} from './page/page.component';
-import {ContactComponent} from './contact/contact.component';
-
-const routes: Routes = [
-  {path: '', component: BlogComponent},
-  {path: 'posts/:id', component: PostDetailComponent},
-  {path: 'page', component: PageComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'posts/:id', component: PostDetailComponent},
-  {path: '404', component: NotFoundComponent},
-  {path: '**', redirectTo: '404'}
-];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [RouterModule]
+  ]
 })
 export class AppRoutingModule { }
